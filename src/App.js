f0be1e275e50bf75465e1bd819ad4127b5c0d8b9
SamEdwards141 <killercustard12@gmail.com>
2023-01-11 14:59:28 +0000

    Setup default + get pokemon from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import Header from "./components/Header";
+import PokemonList from "./components/PokemonList";
+//import { initialURL } from "./PokemonGetter";
+import './styles/css/main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	//const getPokemonURL = useRef(initialURL);
+	const [url, setURL] = useState("https://pokeapi.co/api/v2/pokemon?limit=21");
+	const [nextUrl, setNextURL] = useState();
+	const { ref, inView } = useInView({
+		threshold: 0
+	  });
+	const [pokemon, setPokemon] = useState([]);
+
+	useEffect(() => {
+		const controller = new AbortController();
+		const signal = controller.signal;
+
+		async function getPokemonBatch() {
+			//const data = await fetch(getPokemonURL.current, { signal })
+			const data = await fetch(url, { signal })
+			const { next, results } = await data.json();
+			
+			setNextURL(next);
+			setPokemon(results);
+		}
+		
+		getPokemonBatch();
+
+		return () => controller.abort();
+	}, [url])
+
+	return (
+		<>
+			<Header />
+			<PokemonList pokemon={pokemon} />
+			<div ref={ref} className="hide">1</div>
+		</>
+	);
 }
-
-export default App;
