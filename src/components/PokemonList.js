16f0f5a6d6b430e9c56a621b73ba65b00818aaa8
SamEdwards141 <killercustard12@gmail.com>
2023-01-11 19:38:00 +0000

    Remove unused ref

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,9 +1,8 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { getIndividualPokemon } from "../PokemonGetter";
 
 export default function PokemonList({pokemon}) {
 	const [individualPokemonData, setIndividualPokemonData] = useState([]);
-	const img = useRef(null);
 	
 	useEffect(() => {
 		const urls = pokemon.map(mon => mon.url);
@@ -19,7 +18,7 @@ export default function PokemonList({pokemon}) {
     <div className="list">
 		{individualPokemonData.map(data => { 
 			return <div key={data.id} className="pokemon">
-				<img src={data.sprites.other.dream_world.front_default} alt={data.name + " sprite"} height="100" width="100" ref={img}/>
+				<img src={data.sprites.other.dream_world.front_default} alt={data.name + " sprite"} height="100" width="100"/>
 				<h3>#{data.id < 100 ? 0 : ''}{data.id < 10 ? 0 : ''}{data.id} - {data.name}</h3>
 			</div>
 		})}
