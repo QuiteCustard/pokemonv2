55bf9d950c3c56eff3abc80ca4c3dfd85ce6da03
SamEdwards141 <killercustard12@gmail.com>
2023-01-11 15:01:49 +0000

    Setup default and get mons from API

---
+++ src/PokemonGetter.js
@@ -0,0 +1,7 @@
+const pokemonAmount = 12;
+const baseURL = "https://pokeapi.co/api/v2/";
+
+export const initialURL = `${baseURL}pokemon?limit=${pokemonAmount}`;
+
+export async function getIndividualPokemon(id) {
+}
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+      <header>
+          <img src="" alt="Logo" height="100" width="100"></img>
+          <h1>Pokedex</h1>
+      </header>
+  )
+}
