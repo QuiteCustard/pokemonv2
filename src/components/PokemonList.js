faf22bb7fa305297a9384362a200e0b5953d6bb7
SamEdwards141 <killercustard12@gmail.com>
2023-01-13 15:16:53 +0000

    Switch ternary to display default if alt is selected but alt image doesn't exist

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -17,7 +17,7 @@ export default function PokemonList({pokemon, imgStyle}) {
     <div className="list">
 		{individualPokemonData.map(data => { 
 			return <div key={data.id} className="pokemon">
-				<img src={imgStyle === "default" ? imgStyle === "alt" ? data.sprites.front_default : data.sprites.other.dream_world.front_default : data.sprites.front_default} alt={data.name + " sprite"} height="100" width="100"/>
+				<img src={imgStyle === "default" ? data.sprites.front_default : imgStyle === "alt" && data.sprites.other.dream_world.front_default ? data.sprites.other.dream_world.front_default : data.sprites.front_default} alt={data.name + " sprite"} height="100" width="100"/>
 				<h3>#{data.id < 100 ? data.id < 10 ?  `00${data.id}` : `0${data.id}` : data.id} - {data.name}</h3>
 			</div> 
 		})}
