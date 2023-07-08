187dbe3588f5d1c06adcc3c696b29aa19fcadd63
QuiteCustard <56020602+QuiteCustard@users.noreply.github.com>
2023-07-08 16:16:21 +0100

    Remove unneeded files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { useEffect, useRef, useState } from "react";
+import {
+	useEffect,
+	useRef,
+	useState
+} from "react";
 
 import Header from "./components/Header";
 import PokemonList from "./components/PokemonList";
 import './styles/css/main.css';
-import { useInView } from "react-intersection-observer";
+import {
+	useInView
+} from "react-intersection-observer";
 import logo from "./assets/images/logo.png";
 import Loading from "./components/Loading";
-import { register } from 'swiper/element/bundle';
-import { FreeMode, Mousewheel, Keyboard, Navigation } from 'swiper/modules';
+import {
+	register
+} from 'swiper/element/bundle';
+import {
+	FreeMode,
+	Mousewheel,
+	Keyboard
+} from 'swiper/modules';
 register();
 
 export default function App() {
@@ -16,8 +28,12 @@ export default function App() {
 	const [pokemon, setPokemon] = useState([]);
 	const [pokedexStyle, setpokedexStyle] = useState(localStorage.getItem("pokedex-style") ? localStorage.getItem("pokedex-style") : "gen9");
 	const pokedexButton = useRef(null);
-	const [initLoad, setInitLoad] = useState(false);
-	const { ref, inView } = useInView({rootMargin: "0px 1000px 0px 0px"});
+	const {
+		ref,
+		inView
+	} = useInView({
+		rootMargin: "0px 1000px 0px 0px"
+	});
 	const [loading, setLoading] = useState(true);
 	const swiper = useRef(null);
 	const [initSwiper, setInitSwiper] = useState(false);
@@ -25,45 +41,49 @@ export default function App() {
 	useEffect(() => {
 		const controller = new AbortController();
 		const signal = controller.signal;
-		
+
 		async function getPokemonBatch() {
 			setLoading(true);
-			const data = await fetch(url, { signal })
-			const { next, results } = await data.json();
+			const data = await fetch(url, {
+				signal
+			})
+			const {
+				next,
+				results
+			} = await data.json();
 
-			if (results) { 
+			if (results) {
 				setNextURL(next);
 				setPokemon(p => [...p, ...results]);
-				setInitLoad(true);
 				setInitSwiper(true);
 				setLoading(false);
 			};
 		}
-		
+
 		getPokemonBatch();
 		return () => controller.abort();
 	}, [url])
-	
+
 	useEffect(() => {
-		if (inView && initLoad) setURL(nextUrl);
-	}, [nextUrl, inView, initLoad])
-	
+		if (inView) setURL(nextUrl);
+	}, [nextUrl, inView])
+
 	useEffect(() => {
+		setInitSwiper(true);
 		localStorage.setItem('pokedex-style', pokedexStyle);
 		pokedexButton.current.textContent = `Current Gen: ${pokedexStyle.substring(3)}`;
 		document.body.dataset.theme = pokedexStyle;
 
-		setTimeout(function() {
+		setTimeout(function () {
 			setLoading(false);
 		}, 700)
 	}, [pokedexStyle])
-	
+
 	function changePokedexStyle() {
 		setLoading(true);
-		setTimeout(function() {
+		setTimeout(function () {
 			if (pokedexStyle === "gen9") setpokedexStyle("gen4");
 			else setpokedexStyle("gen9");
-			setInitSwiper(true);
 		}, 1500)
 	}
 
@@ -77,20 +97,27 @@ export default function App() {
 					freemode: true,
 					spaceBetween: 20,
 					breakpoints: {
-						640: {
+						320: {
+							slidesPerView: 2
+						},
+						559: {
 							slidesPerView: 4
 						},
 						1024: {
-						  slidesPerView: 8,
+							slidesPerView: 6,
 						},
-					  },
-					on: {
-						init() {
+						1400: {
+							slidesPerView: 8,
+						},
+						2000: {
+							slidesPerView: 10,
 						},
 					},
+					on: {
+						init() {},
+					},
 				}
-			}
-			else {
+			} else {
 				return {
 					direction: "vertical",
 					mousewheel: true,
@@ -101,16 +128,12 @@ export default function App() {
 						320: {
 							slidesPerView: 1
 						},
-						640: {
-							slidesPerView: 2
-						},
 						1024: {
-						  slidesPerView: 6,
+							slidesPerView: 6,
 						},
-					  },
+					},
 					on: {
-						init() {
-						},
+						init() {},
 					},
 				}
 			}
@@ -118,27 +141,52 @@ export default function App() {
 
 		if (initSwiper === true) {
 			const swiperParams = getSwiperParams();
-	
+			console.log(pokedexStyle, swiperParams)
 			Object.assign(swiper.current, swiperParams);
 			swiper.current.initialize();
 		}
-		
+
 		setInitSwiper(false);
-	}, [initSwiper, pokedexStyle])
-	
+	}, [initSwiper])
+
 	useEffect(() => {
 		swiper.current.addEventListener('slidechange', (e) => {
 			swiper.current.querySelectorAll("swiper-slide").forEach(slide => {
 				if (slide.role === null) setInitSwiper(true);
 			})
-		  });
+		});
 	}, [])
 
-	return (
-		<>
-			<Header pokedexStyle={pokedexStyle ? changePokedexStyle : null} pokedexButton={pokedexButton} logo={logo}/>
-			<PokemonList pokemon={pokemon} pokedexStyle={pokedexStyle} listRef={ref} swiper={swiper}/>
-			<Loading loading={loading} />
-		</>
+	return ( <
+		>
+		<
+		Header pokedexStyle = {
+			pokedexStyle ? changePokedexStyle : null
+		}
+		pokedexButton = {
+			pokedexButton
+		}
+		logo = {
+			logo
+		}
+		/> <
+		PokemonList pokemon = {
+			pokemon
+		}
+		pokedexStyle = {
+			pokedexStyle
+		}
+		listRef = {
+			ref
+		}
+		swiper = {
+			swiper
+		}
+		/> <
+		Loading loading = {
+			loading
+		}
+		/> <
+		/>
 	);
 }
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -17,14 +17,14 @@ export default function PokemonList({pokemon, pokedexStyle, listRef, swiper}) {
 
   return (
 	<div className="pokemon-list">
-		<swiper-container init="false" navigation="true" ref={swiper}>
+		<swiper-container init="false" ref={swiper}>
 			{individualPokemonData.map(data => { 
 				return <swiper-slide key={data.id}>
 					<div className="pokemon">
 						<img className="pokemon-img" src={data.sprites.other.dream_world.front_default ? data.sprites.other.dream_world.front_default : data.sprites.front_default} alt={data.name + " sprite"} width={pokedexStyle === "gen4" ? "452" : "80"} height={pokedexStyle === "gen4" ? "439" : "80"}/>
 						<div className="name-id">
 							<h3 className="pokemon-name">{data.name}</h3>
-							<p className="pokemon-id">{pokedexStyle === "gen4" ? "0" + data.id : data.id}</p>
+							<p className="pokemon-id">{pokedexStyle === "gen4" && data.id < 100 ? "0" : ''}{pokedexStyle === "gen4" && data.id < 10 ? "0" + data.id : data.id}</p>
 						</div>
 					</div>
 				</swiper-slide> 
