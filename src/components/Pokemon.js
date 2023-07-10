613c5cd7b3f41ec44980430ef01fe16144a417a1
QuiteCustard <56020602+QuiteCustard@users.noreply.github.com>
2023-07-10 21:19:25 +0100

    Add "observer"

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,15 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useRef,  } from "react";
 import { useInView } from "react-intersection-observer";
 
-export default function Pokemon({data, pokedexStyle, pokemonObserver}) {
-    const [ pokemon, inView, entry ] = useInView({rootMargin: "0px 0px 0px 0px", root: pokemonObserver.current});
-
-    useEffect(() => {
-        console.log(inView, entry);
-    }, [inView])
-
+export default function Pokemon({data, pokedexStyle}) {
     return (
-        <div className="pokemon" ref={pokemon}>
+        <div className="pokemon">
             <img className="pokemon-img" src={data.sprites.other.dream_world.front_default ? data.sprites.other.dream_world.front_default : data.sprites.front_default} alt={data.name + " sprite"} width={pokedexStyle === "gen4" ? "452" : "80"} height={pokedexStyle === "gen4" ? "439" : "80"}/>
             <div className="name-id">
                 <h3 className="pokemon-name">{data.name}</h3>
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { getIndividualPokemon } from "../PokemonGetter";
 import Pokemon from "./Pokemon";
 
 export default function PokemonList({pokemon, pokedexStyle, listRef, listScroller, pokemonObserver}) {
 	const [individualPokemonData, setIndividualPokemonData] = useState([]);
+	const [monList, setMonList] = useState(false);
 
 	useEffect(() => {
 		const urls = pokemon.map(mon => mon.url);
@@ -14,12 +15,30 @@ export default function PokemonList({pokemon, pokedexStyle, listRef, listScrolle
 		}
 
 		getData();
+
+		function checkOverlap(mon) {
+			const observerCords = pokemonObserver.current.getBoundingClientRect();
+			const monCords = mon.getBoundingClientRect();
+	
+			if ((observerCords.top + observerCords.height) < monCords.top || observerCords.top > (monCords.top + monCords.height) || (observerCords.left + observerCords.width) < monCords.left || observerCords.left > (monCords.left + monCords.width)) pokemonObserver.current.dataset.tester = mon.querySelector("img").src;
+		}
+
+		if (monList === true) {
+			listScroller.current.addEventListener("scroll", () => {
+				listScroller.current.querySelectorAll(".pokemon").forEach(mon => checkOverlap(mon));
+			})
+		}
 	}, [pokemon])
+	
+
+    useEffect(() => {
+		if (listScroller.current !== null) setMonList(true);
+    }, [listScroller])
 
   return (
 	<div className="pokemon-list" ref={listScroller}>
 		{individualPokemonData.map((data) => { 
-			return <Pokemon key={data.id} data={data} pokedexStyle={pokedexStyle} pokemonObserver={pokemonObserver} />
+			return <Pokemon key={data.id} data={data} pokedexStyle={pokedexStyle}/>
 		})}
 		<div ref={listRef}></div>
 	</div>
